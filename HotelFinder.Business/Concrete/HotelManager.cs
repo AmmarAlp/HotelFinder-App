1da8e6971f799821f1e907e350ea6a3e96808d50
M A Alp K <muarkrky@gmail.com>
2024-08-31 16:27:40 +0300

    Add project files.

---
+++ HotelFinder.Business/Concrete/HotelManager.cs
@@ -0,0 +1,55 @@
+﻿using HotelFinder.Business.Abstract;
+using HotelFinder.Entities;
+using HotelFinder.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HotelFinder.DataAccess.Abstract;
+using HotelFinder.DataAccess.Concrete;
+
+namespace HotelFinder.Business.Concrete
+{
+    
+    public class HotelManager : IHotelService
+    {
+        private IHotelRepository _hotelRepository;
+
+        public HotelManager(IHotelRepository hotelRepository) 
+        {
+            _hotelRepository = hotelRepository;
+        }
+        
+        public Hotel CreateHotel(Hotel hotel)
+        {
+            return _hotelRepository.CreateHotel(hotel);
+        }
+
+        public void DeleteHotel(int id)
+        {
+            _hotelRepository.DeleteHotel(id);
+        }
+
+        public List<Hotel> GetAllHotels()
+        {
+            return _hotelRepository.GetAllHotels();
+        }
+
+        public Hotel GetHotelById(int id)
+        {
+            if (id > 0)
+            {
+                return _hotelRepository.GetHotelById(id);
+            }
+
+            throw new Exception("id can not be less than one");
+            
+        }
+
+        public Hotel UpdateHotel(Hotel hotel)
+        {
+            return _hotelRepository.UpdateHotel(hotel);
+        }
+    }
+}
