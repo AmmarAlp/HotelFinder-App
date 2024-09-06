f3c5aa6259dce33431f9abc3e58688dc9c890efc
M A Alp K <muarkrky@gmail.com>
2024-09-06 16:12:13 +0300

    Adding Swagger Config

--- HotelFinder.API/Program.cs
+++ HotelFinder.API/Program.cs
@@ -2,6 +2,8 @@ using HotelFinder.Business.Abstract;
 using HotelFinder.Business.Concrete;
 using HotelFinder.DataAccess.Abstract;
 using HotelFinder.DataAccess.Concrete;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +11,23 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddSingleton<IHotelService, HotelManager>();
 builder.Services.AddSingleton<IHotelRepository, HotelRepository>();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerDocument(config =>
+{
+    config.PostProcess = (doc =>
+    {
+        doc.Info.Title = "All Hotels Api";
+        doc.Info.Version = "1.013";
+        doc.Info.Contact = new NSwag.OpenApiContact()
+        {
+            Name = "M.A. Alp Karakaya",
+            Url = "https://www.linkedin.com/in/alp-krky/",
+            Email = "muarkrky@gmail.com"
+        };
+    });
+});
+
+
 
 var app = builder.Build();
 
@@ -20,6 +39,9 @@ if (!app.Environment.IsDevelopment())
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseOpenApi();
+app.UseSwaggerUi();
 
 app.UseAuthorization();
 
